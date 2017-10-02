c1913c3394f29c4610ef66b7b0fee305ed953ea1
softwareandoutsourcing <nomail@nomail.org>
2017-10-02 20:01:18 +0200

    Adds lazy loading demo using jQuery.

--- custom-webpack/src/app/+lazy/lazy.module.ts
+++ custom-webpack/src/app/+lazy/lazy.module.ts
@@ -1,11 +1,20 @@
-import {NgModule, Component} from '@angular/core'
+import {NgModule, Component, Inject, PLATFORM_ID} from '@angular/core'
 import {RouterModule} from '@angular/router'
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'lazy-view',
-  template: `<h3>i'm lazy</h3>`
+  template: `<h3>i'm lazy</h3><div id="jquery-text">jQuery is not working</div>`
 })
-export class LazyView {}
+export class LazyView {
+  constructor( @Inject(PLATFORM_ID) private platformId: Object) {}
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      let $ = require('jquery');
+      $('#jquery-text').text('jQuery works!');
+    }
+  }
+}
 
 @NgModule({
   declarations: [LazyView],
